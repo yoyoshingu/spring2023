56bd2c1d860392404fa479a519c4ee5d10ace531
yoyoshingu <leo@shingu.ac.kr>
2023-01-16 17:49:20 +0900

    1st happy

---
+++ jblogp/src/main/java/com/leo/spring1/JblogpApplication.java
@@ -0,0 +1,13 @@
+package com.leo.spring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JblogpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JblogpApplication.class, args);
+	}
+
+}
