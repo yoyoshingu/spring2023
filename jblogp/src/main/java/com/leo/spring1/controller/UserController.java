ff0eac747020c8d2cd707b984cdbf03e4aafe27c
yoyoshingu <leo@shingu.ac.kr>
2023-01-19 14:02:38 +0900

    before exception

--- jblogp/src/main/java/com/leo/spring1/controller/UserController.java
+++ jblogp/src/main/java/com/leo/spring1/controller/UserController.java
@@ -2,6 +2,8 @@ package com.leo.spring1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,5 +22,11 @@ public class UserController {
 		user.setRole(RoleType.USER);
 		userRepository.save(user);
 		return user.getUsername() + " 회원가입 성공";
+	}
+	
+	@GetMapping("/user/get/{id}")
+	public @ResponseBody User getUser(@PathVariable int id) {
+		User findUser = userRepository.findById(id).get();
+		return findUser;
 	}
 }
---
+++ jblogp/src/main/java/com/leo/spring1/exception/Spring1Exception.java
@@ -0,0 +1,10 @@
+package com.leo.spring1.exception;
+
+public class Spring1Exception extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public Spring1Exception(String message) {
+		super(message);
+	}
+
+}
